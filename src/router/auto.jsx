bb10a48be706a38e64b7ab6c2cd221d20ebba2f4
rinba <q3255620496@outlook.com>
2023-11-20 20:49:10 +0800

    运行

--- src/router/auto.jsx
+++ src/router/auto.jsx
@@ -4,15 +4,21 @@
 
       
 const routes = [{
-  [MENU_PATH]: "/details/person",
-  components: () => import("../pages/details/person.tsx")
-}, {
   [MENU_PATH]: "/form/index",
   [MENU_LAYOUT]: 'FULLSCREEN',
   components: () => import("../pages/form/index.tsx")
 }, {
+  [MENU_PATH]: "/details/person",
+  components: () => import("../pages/details/person.tsx")
+}, {
   [MENU_PATH]: "/icons",
   components: () => import("../pages/icons/index.tsx")
+}, {
+  [MENU_PATH]: "/list/card",
+  components: () => import("../pages/list/card.tsx")
+}, {
+  [MENU_PATH]: "/list/search",
+  components: () => import("../pages/list/search.tsx")
 }, {
   [MENU_PATH]: "/power/menu",
   components: () => import("../pages/power/menu.tsx")
@@ -22,12 +28,6 @@ const routes = [{
 }, {
   [MENU_PATH]: "/power/user",
   components: () => import("../pages/power/user.tsx")
-}, {
-  [MENU_PATH]: "/list/card",
-  components: () => import("../pages/list/card.tsx")
-}, {
-  [MENU_PATH]: "/list/search",
-  components: () => import("../pages/list/search.tsx")
 }, {
   [MENU_PATH]: "/statistics/feedback",
   components: () => import("../pages/statistics/feedback.tsx")
