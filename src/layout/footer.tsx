1110c6f92beabeb0d76037d5f918870bab08fa27
rinba <q3255620496@outlook.com>
2023-11-21 09:37:04 +0800

    更新footer

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -4,9 +4,9 @@ const { Footer } = Layout;
 export default function BottomFooter() {
   return (
     <Footer className="footer">
-      <p>......我可是有底线的......</p>
-      <p>备案/许可证编号：陕ICP备00000000号</p>
-      <p>react-ant-admin ©2023 Created by rinba_murphy</p>
+      <p>...我是有底线的...</p>
+      <p>备案/许可证编号：吉ICP备00000000号</p>
+      <p>实验室级后台管理系统 ©2023 Created by Code Academy Lab</p>
     </Footer>
   );
 }
